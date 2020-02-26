324d4b026971a135c4d9a609f285f2a485ab7195
Hayley Hollermann <hayleyhollermann@gmail.com>
2020-02-26 11:02:45 -0600

    determined min and max values for byte

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,16 +3,25 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        
+        // INT
         int myValue = 10000;
-
         int myMinIntValue = Integer.MIN_VALUE;
         int myMaxIntValue = Integer.MAX_VALUE;
-
         System.out.println("Integer min value = " + myMinIntValue);
         System.out.println("Integer max value = " + myMaxIntValue);
-
         System.out.println("Busted MAX value = " + (myMaxIntValue + 1)); // overflow
         System.out.println("Busted Min value = " + (myMinIntValue - 1)); // overflow
+        int myMaxIntTest = 2147483647; // if you type a literal number larger than the max, you get an error in intellij
+        int myMaxIntWithUnderscores = 2_147_483_647; // underscores are used in place of commas (if that is easier on the eyes)
+
+        // BYTE
+        byte myMinByteValue = Byte.MIN_VALUE;
+        byte myMaxByteValue = Byte.MAX_VALUE;
+        System.out.println("byte min value = " + myMinByteValue);
+        System.out.println("byte max value = " + myMaxByteValue);
+
+
 
     }
 }
