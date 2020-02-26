64a1bc521fc902c08b5f1efcca4ae5831cdcd1c8
Hayley Hollermann <hayleyhollermann@gmail.com>
2020-02-26 10:51:58 -0600

    determined min and max int values

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
         int myValue = 10000;
+
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
+
+        System.out.println("Integer min value = " + myMinIntValue);
+        System.out.println("Integer max value = " + myMaxIntValue);
+
+        System.out.println("Busted MAX value = " + (myMaxIntValue + 1)); // overflow
+        System.out.println("Busted Min value = " + (myMinIntValue - 1)); // overflow
+
     }
 }
